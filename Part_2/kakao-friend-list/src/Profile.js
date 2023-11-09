d6dee89ead965aa1476b8a170228ee2c028e342a
Jinoo <jw9622@gmail.com>
2023-11-09 17:06:26 +0900

    Component styling: Inline, StyleSheet, using styled-component

--- Part_2/kakao-friend-list/src/Profile.js
+++ Part_2/kakao-friend-list/src/Profile.js
@@ -1,32 +1,43 @@
 import { Image, View, Text } from "react-native";
 import Margin from "./Margin";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex-direction: row;
+  background-color: #fff;
+`;
+const ProfileImage = styled.Image`
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
+  border-radius: ${(props) => props.size * 0.4}px;
+`;
+const TextContainer = styled.View`
+  justify-content: center;
+  margin-left: 10px;
+`;
+const NameText = styled.Text`
+  font-weight: ${(props) => (props.isMyProfile ? "bold" : "normal")};
+  font-size: ${(props) => (props.isMyProfile ? 16 : 14)}px;
+`;
+const IntroductionText = styled.Text`
+  font-size: ${(props) => (props.isMyProfile ? 12 : 11)}px;
+  color: gray;
+`;
 
 export default ({ uri, name, introduction, isMyProfile }) => {
   const size = isMyProfile ? 50 : 40;
   return (
-    <View style={{ flexDirection: "row" }}>
-      <Image
-        source={{ uri: uri }}
-        style={{ width: size, height: size, borderRadius: size * 0.4 }}
-      />
-      <View style={{ justifyContent: "center", marginLeft: 10 }}>
-        <Text
-          style={{
-            fontWeight: isMyProfile ? "bold" : undefined,
-            fontSize: isMyProfile ? 16 : 14,
-          }}
-        >
-          {name}
-        </Text>
+    <Container>
+      <ProfileImage source={{ uri: uri }} size={size} />
+      <TextContainer>
+        <NameText isMyProfile={isMyProfile}>{name}</NameText>
         {!!introduction && (
           <View>
             <Margin height={isMyProfile ? 6 : 2} />
-            <Text style={{ fontSize: isMyProfile ? 12 : 11, color: "grey" }}>
-              {introduction}
-            </Text>
+            <IntroductionText>{introduction}</IntroductionText>
           </View>
         )}
-      </View>
-    </View>
+      </TextContainer>
+    </Container>
   );
 };
