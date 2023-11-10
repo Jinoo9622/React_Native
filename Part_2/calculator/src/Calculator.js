ebb290904a6d5a54e46e3f671beae335720800aa
Jinoo <jw9622@gmail.com>
2023-11-10 14:51:08 +0900

    Calculator UI

---
+++ Part_2/calculator/src/Calculator.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import styled from "styled-components";
+
+const COLOR = {
+  RESULT: "#4e4c51",
+  RESET: "#5f5e62",
+  OPERATOR: "#f39c29",
+  NUM: "#5c5674",
+};
+// Button type: 'reset' | 'operator' | 'num'
+const Button = ({ text, onPress, flex, type }) => {
+  const backgroundColor =
+    type === "reset"
+      ? COLOR.RESET
+      : type === "operator"
+      ? COLOR.OPERATOR
+      : type === "num"
+      ? COLOR.NUM
+      : "transparent";
+  return (
+    <TouchableOpacity
+      style={{
+        flex,
+        backgroundColor,
+        justifyContent: "center",
+        alignItems: "center",
+        // paddingV ertical: 10,
+        height: 50,
+        borderWidth: 0.3,
+        borderColor: "black",
+      }}
+      onPress={onPress}
+    >
+      <Text style={{ color: "white", fontSize: 25 }}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const ButtonContainer = styled.View`
+  flex-direction: row;
+  width: 100%;
+`;
+const InputContainer = styled.View`
+  background-color: ${COLOR.RESULT};
+  min-height: 50px;
+  justify-content: center;
+  align-items: flex-end;
+  /* padding: 5px;  // top, right, bottom, left  */
+  padding: 10px 0px; /* top, bottom(vertical) 10px | right, left(horizontal) 0px */
+  /* padding: 1px 2px 3px 4px; // top, right, bottom, left */
+`;
+
+export default () => {
+  const [input, setInput] = useState(0);
+  const [currentOperator, setCurrentOperator] = useState(null);
+  const [result, setResult] = useState(null);
+  const [tempInput, setTempInput] = useState(null);
+  const [tempOperator, setTempOperator] = useState(null);
+  return (
+    // flex: 부모 component로부터 받은 비율을 나눔
+    <View style={{ flex: 1, width: 250, justifyContent: "center" }}>
+      {/* 결과 */}
+      <InputContainer>
+        <Text style={{ color: "white", fontSize: 35, textAlign: "right" }}>
+          {input}
+        </Text>
+      </InputContainer>
+
+      {/* [AC ~ /] */}
+      <ButtonContainer style={{ flexDirection: "row", width: "100%" }}>
+        <Button type="reset" text="AC" onPress={() => null} flex={3} />
+        <Button type="operator" text="/" onPress={() => null} flex={1} />
+      </ButtonContainer>
+
+      {/* [7 ~ x] */}
+      <ButtonContainer style={{ flexDirection: "row", width: "100%" }}>
+        <Button type="num" text="7" onPress={() => null} flex={1} />
+        <Button type="num" text="8" onPress={() => null} flex={1} />
+        <Button type="num" text="9 " onPress={() => null} flex={1} />
+        <Button type="operator" text="X" onPress={() => null} flex={1} />
+      </ButtonContainer>
+
+      {/* [4 ~ -] */}
+      <ButtonContainer style={{ flexDirection: "row", width: "100%" }}>
+        <Button type="num" text="4" onPress={() => null} flex={1} />
+        <Button type="num" text="5" onPress={() => null} flex={1} />
+        <Button type="num" text="6 " onPress={() => null} flex={1} />
+        <Button type="operator" text="-" onPress={() => null} flex={1} />
+      </ButtonContainer>
+
+      {/* [1 ~ +] */}
+      <ButtonContainer style={{ flexDirection: "row", width: "100%" }}>
+        <Button type="num" text="1" onPress={() => null} flex={1} />
+        <Button type="num" text="2" onPress={() => null} flex={1} />
+        <Button type="num" text="3 " onPress={() => null} flex={1} />
+        <Button type="operator" text="+" onPress={() => null} flex={1} />
+      </ButtonContainer>
+
+      {/* [0 ~ =] */}
+      <ButtonContainer style={{ flexDirection: "row", width: "100%" }}>
+        <Button type="num" text="0" onPress={() => null} flex={3} />
+        <Button type="operator" text="= " onPress={() => null} flex={1} />
+      </ButtonContainer>
+    </View>
+  );
+};
