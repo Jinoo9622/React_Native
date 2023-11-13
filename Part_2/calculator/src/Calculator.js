468a50ae2406a663dc329760c084289bcb7d672c
Jinoo <jw9622@gmail.com>
2023-11-13 09:20:49 +0900

    Add Custom hook

--- Part_2/calculator/src/Calculator.js
+++ Part_2/calculator/src/Calculator.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import styled from "styled-components";
+import { useCalculator } from "./use-calculator";
 
 const COLOR = {
   RESULT: "#4e4c51",
@@ -52,74 +53,17 @@ const InputContainer = styled.View`
 `;
 
 export default () => {
-  const [input, setInput] = useState(0);
-  const [currentOperator, setCurrentOperator] = useState(null);
-  const [result, setResult] = useState(null);
-  const [tempInput, setTempInput] = useState(null);
-  const [tempOperator, setTempOperator] = useState(null);
-  const [isClickedOperator, setIsClickedOperator] = useState(false);
-  const [isClickedEqual, setIsClickedEqual] = useState(false);
-
-  // const hasInput = input ? true : false; ==
-  const hasInput = !!input;
-
-  const onPressNum = (num) => {
-    if (currentOperator && isClickedOperator) {
-      setResult(input);
-      setInput(num);
-      setIsClickedOperator(false); // 초기화
-    } else {
-      const newInput = Number(`${input}${num}`);
-      setInput(newInput);
-    }
-  };
-
-  const onPressOperator = (operator) => {
-    if (operator !== "=") {
-      setCurrentOperator(operator);
-      setIsClickedOperator(true);
-      setIsClickedEqual(false);
-    } else {
-      // =
-      let finalResult = result;
-      const finalInput = isClickedEqual ? tempInput : input;
-      const finalOperator = isClickedEqual ? tempOperator : currentOperator;
-      switch (finalOperator) {
-        case "+":
-          finalResult = result + finalInput;
-          break;
-        case "-":
-          finalResult = result - finalInput;
-          break;
-        case "*":
-          finalResult = result * finalInput;
-          break;
-        case "/":
-          finalResult = result / finalInput;
-          break;
-        default:
-          break;
-      }
-      setResult(finalResult);
-      setInput(finalResult);
-      setTempInput(finalInput);
-      setCurrentOperator(null);
-      setTempOperator(finalOperator);
-      setIsClickedEqual(true);
-    }
-  };
-
-  const onPressReset = () => {
-    if (hasInput) {
-      setInput(0);
-    } else {
-      setInput(0);
-      setCurrentOperator(null);
-      setResult(null);
-      setTempInput(null);
-      setTempOperator(null);
-    }
-  };
+  const {
+    input,
+    currentOperator,
+    result,
+    tempInput,
+    tempOperator,
+    hasInput,
+    onPressNum,
+    onPressOperator,
+    onPressReset,
+  } = useCalculator();
 
   return (
     // flex: 부모 component로부터 받은 비율을 나눔
