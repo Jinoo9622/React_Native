1b75f8e0b71ccff19ab9026d7576c0f49e660f51
Jinoo <jw9622@gmail.com>
2023-11-14 10:56:53 +0900

    display selected date

--- Part_2/todo-and-calender/App.js
+++ Part_2/todo-and-calender/App.js
@@ -1,17 +1,131 @@
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { getCalendarColumns, getDayColor, getDayText } from "./src/utils";
 import { runPracticeDayjs } from "./src/practice-dayjs";
+import dayjs from "dayjs";
+import Margin from "./src/Margin";
+import { SimpleLineIcons } from "@expo/vector-icons";
+
+const columnSize = 35;
+const Column = ({ text, color, opacity, disabled, onPress, isSelected }) => {
+  return (
+    <TouchableOpacity
+      disabled={disabled}
+      onPress={onPress}
+      style={{
+        width: columnSize,
+        height: columnSize,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: isSelected ? "#c2c2c2" : "transparent",
+        borderRadius: columnSize / 2,
+      }}
+    >
+      <Text style={{ color: color, opacity: opacity }}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const ArrowButton = ({ iconName, onPress }) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{ paddingHorizontal: 20, paddingVertical: 15 }}
+    >
+      <SimpleLineIcons name={iconName} size={15} color="#404040" />
+    </TouchableOpacity>
+  );
+};
 
 export default function App() {
+  const now = dayjs();
+  const [selectedDate, setSelectedDate] = useState(now);
+  const columns = getCalendarColumns(selectedDate);
+
+  const ListHeaderComponent = () => {
+    const currentDateText = dayjs(selectedDate).format("YYYY.MM.DD.");
+    return (
+      <View>
+        {/* <YYYY.MM.DD> */}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ArrowButton iconName={"arrow-left"} onPress={() => {}} />
+          <TouchableOpacity>
+            <Text style={{ fontSize: 20, color: "#404040" }}>
+              {currentDateText}
+            </Text>
+          </TouchableOpacity>
+          <ArrowButton iconName={"arrow-right"} onPress={() => {}} />
+        </View>
+        {/* 일 ~  토 */}
+        <View style={{ flexDirection: "row" }}>
+          {[0, 1, 2, 3, 4, 5, 6].map((day) => {
+            const dayText = getDayText(day);
+            const color = getDayColor(day);
+            return (
+              <Column
+                key={`day-${day}`}
+                text={dayText}
+                color={color}
+                opacity={1}
+                disabled={true}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  };
+  const renderItem = ({ item: date }) => {
+    const dateText = dayjs(date).get("date");
+    const day = dayjs(date).get("day");
+    const color = getDayColor(day);
+    const isCurrentMonth = dayjs(date).isSame(selectedDate, "month");
+    const onPress = () => {
+      setSelectedDate(date);
+    };
+    const isSelected = dayjs(date).isSame(selectedDate, "date");
+    return (
+      <Column
+        text={dateText}
+        color={color}
+        opacity={isCurrentMonth ? 1 : 0.4}
+        onPress={onPress}
+        isSelected={isSelected}
+      />
+    );
+  };
   useEffect(() => {
-    runPracticeDayjs();
+    // runPracticeDayjs();
   }, []);
+  useEffect(() => {
+    console.log(
+      "selectedDate: ",
+      dayjs(selectedDate).format("YYYY.MM.DD HH:mm:ss")
+    );
+  }, [selectedDate]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={columns}
+        keyExtractor={(_, index) => `column-${index}`}
+        numColumns={7}
+        renderItem={renderItem}
+        ListHeaderComponent={ListHeaderComponent}
+      />
+    </SafeAreaView>
   );
 }
 
