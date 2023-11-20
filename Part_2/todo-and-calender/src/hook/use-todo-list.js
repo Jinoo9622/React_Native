aa9394495b0d3912dc9e8b4294a6538f50a700a1
Jinoo <jw9622@gmail.com>
2023-11-20 13:37:00 +0900

    asyncstorage

--- Part_2/todo-and-calender/src/hook/use-todo-list.js
+++ Part_2/todo-and-calender/src/hook/use-todo-list.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const defaultTodoList = [
   // {
@@ -22,9 +23,17 @@ const defaultTodoList = [
   // },
 ];
 
+const TODO_LIST_KEY = "TODO_LIST_KEY";
+
 export const useTodoList = (selectedDate) => {
   const [todoList, setTodoList] = useState(defaultTodoList);
   const [input, setInput] = useState("");
+
+  const saveTodoList = (newTodoList) => {
+    setTodoList(newTodoList);
+    // setItem(key, value): value는 string 이어야만함
+    AsyncStorage.setItem(TODO_LIST_KEY, JSON.stringify(newTodoList));
+  };
 
   const addTodo = () => {
     if (input === "") return;
@@ -41,12 +50,12 @@ export const useTodoList = (selectedDate) => {
         isSuccess: false,
       },
     ];
-    setTodoList(newTodoList);
+    saveTodoList(newTodoList);
   };
 
   const removeTodo = (todoId) => {
     const newTodoList = todoList.filter((todo) => todo.id !== todoId);
-    setTodoList(newTodoList);
+    saveTodoList(newTodoList);
   };
 
   const toggleTodo = (todoId) => {
@@ -57,7 +66,7 @@ export const useTodoList = (selectedDate) => {
         isSuccess: !todo.isSuccess,
       };
     });
-    setTodoList(newTodoList);
+    saveTodoList(newTodoList);
   };
 
   const resetInput = () => setInput("");
@@ -66,6 +75,21 @@ export const useTodoList = (selectedDate) => {
     const isSameDate = dayjs(todo.date).isSame(selectedDate, "date");
     return isSameDate;
   });
+
+  // didMount 시점에 init 실행
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    const result = await AsyncStorage.getItem(TODO_LIST_KEY);
+    if (result) {
+      const newTodoList = JSON.parse(result);
+      // console.log("result", typeof result, result);
+      // console.log("newTodoList", typeof newTodoList, newTodoList);
+      setTodoList(newTodoList);
+    }
+  };
 
   return {
     todoList,
